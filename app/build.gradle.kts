9817d7a7cb931c2b4f6875cbbd55cdd4ee82ea8b
Gilfoyle <gilfoyle@users.noreply.github.com>
2026-09-04 23:42:00 +0000

    Bump to 0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.tide.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.5"
+        versionCode = 6
+        versionName = "0.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
